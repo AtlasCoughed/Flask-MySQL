75ba0c26fd8f4be25a9c0381903dea895e567d2d
Michael Tran <michaelhuytran@gmail.com>
2017-09-26 18:44:07 -0700

    .

--- RestAPI/api.py
+++ RestAPI/api.py
@@ -12,25 +12,38 @@ app.config['MYSQL_DATABASE_PASSWORD'] = '12345'
 app.config['MYSQL_DATABASE_DB'] = 'ItemListDb'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
+# Initalize the mySql instance
 mysql.init_app(app)
 
+# we later add the api routes
 api = Api(app)
 
-
+# Creation of our Class / Method
 class CreateUser(Resource):
     def post(self):
         try:
             # Parse the arguments
             parser = reqparse.RequestParser()
+
+            # Help = error message when a type error is raised while parsing it
             parser.add_argument('email', type=str, help='Email address to create user')
             parser.add_argument('password', type=str, help='Password to create user')
             args = parser.parse_args()
 
+            # Variables referencing the dict ['email'] or ['password']
             _userEmail = args['email']
             _userPassword = args['password']
 
             conn = mysql.connect()
+            # In computer science and technology, a database cursor is a control structure
+            # that enables traversal over the records in a database.
+            # Cursors facilitate subsequent processing in conjunction with the traversal,
+            # such as retrieval, addition and removal of database records. The database cursor characteristic
+            # of traversal makes cursors akin to the programming language concept of iterator.
+
             cursor = conn.cursor()
+
+            # This calls the stored procedures.
             cursor.callproc('spCreateUser', (_userEmail, _userPassword))
             data = cursor.fetchall()
 
