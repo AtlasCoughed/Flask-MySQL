da426e352517cf7c0ece99dafe6113f13856c47e
Michael Tran <michaelhuytran@gmail.com>
2017-09-26 18:11:17 -0700

    .

--- RestAPI/api.py
+++ RestAPI/api.py
@@ -1,12 +1,47 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful import reqparse
+from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '12345'
+app.config['MYSQL_DATABASE_DB'] = 'ItemListDb'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
 api = Api(app)
+
 
 class CreateUser(Resource):
     def post(self):
-        return {'status': 'success'}
+        try:
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('email', type=str, help='Email address to create user')
+            parser.add_argument('password', type=str, help='Password to create user')
+            args = parser.parse_args()
+
+            _userEmail = args['email']
+            _userPassword = args['password']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('spCreateUser', (_userEmail, _userPassword))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return {'StatusCode': '200', 'Message': 'User creation success'}
+            else:
+                return {'StatusCode': '1000', 'Message': str(data[0])}
+
+        except Exception as e:
+            return {'error': str(e)}
 
 api.add_resource(CreateUser, '/CreateUser')
 
